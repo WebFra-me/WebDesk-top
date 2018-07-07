c8a602cc4e587e0f8f17d80803940e5ce2be6a68
Webdesk-me <info@webdesk.me>
2018-07-07 07:42:37 -0400

    style.css upgrade

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ for (i = 0; i < apps.length; i++) {
   if(apps[i] != ".DS_Store"){
   app = fs.readFileSync("Apps/" + apps[i] + "/Admin/app.json");
   oapp = JSON.parse(app);
-    document.getElementById("apps").innerHTML += '<div class="card text-white" style="background-color: #D98911;"><div class="card-body"><h4 class="card-title">' + oapp.title + '</h4><a class="btn btn-primary" href="Apps/' + apps[i] + '/index.html" target="_blank"><b>Open</b></a></div></div>';
+    document.getElementById("apps").innerHTML += '<div class="card"><div class="card-body"><h4 class="card-title">' + oapp.title + '</h4><a class="btn btn-primary" href="Apps/' + apps[i] + '/index.html" target="_blank"><b>Open</b></a></div></div>';
   }
 }
 });
