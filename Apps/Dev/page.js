b5fe8feb887a7587a6b6719a4a528b77b92f032d
Webdesk-me <info@webdesk.me>
2018-07-07 22:23:50 -0400

    Dev Page

--- Apps/Dev/page.js
+++ Apps/Dev/page.js
@@ -5,4 +5,27 @@ $(document).ready(function() {
   var id = url.searchParams.get("app");
   var page = url.searchParams.get("page");
   document.getElementById("path").innerHTML = id + "/" + page;
+  bob = fs.readFileSync("Apps/" + id + "/" + page);
+  document.getElementById("con").innerHTML = bob;
+//CodeMirror
+var myCodeMirror = CodeMirror.fromTextArea(con, {
+lineNumbers: true,
+  mode:  "javascript",
+  theme: "abcdef",
+matchBrackets: true,
+matchTags: {bothTags: true},
+lineWrapping: true,
+foldGutter: true,
+gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter", "CodeMirror-lint-markers"],
+lint: true,
+extraKeys: {"Ctrl-Space": "autocomplete",
+"F11": function(cm) {
+          cm.setOption("fullScreen", !cm.getOption("fullScreen"));
+        },
+        "Esc": function(cm) {
+          if (cm.getOption("fullScreen")) cm.setOption("fullScreen", false);
+        },
+"Ctrl-J": "toMatchingTag"
+}
+});
 });
