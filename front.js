cd9fef1369e0e0d34eee37d4693513bd168bfc1d
Webdesk-me <info@webdesk.me>
2018-07-09 07:06:44 -0400

    Banner Upgrade

--- front.js
+++ front.js
@@ -15,11 +15,20 @@ $(document).ready(function() {
   else{
     var wd_head = '<title>WebDesk-top</title><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"><link rel="stylesheet" href="Plugins/bootstrap/dist/css/bootstrap.min.css"><link rel="stylesheet" href="style.css">';
   }
+  if (fs.existsSync(wd_dir + 'Apps/' + wd_app + '/banner_' + wd_sec + '.html')) {
+    var wd_banner = fs.readFileSync(wd_dir + 'Apps/' + wd_app + '/banner_' + wd_sec + '.html');
+  }
+  else if (fs.existsSync(wd_dir + 'Apps/' + wd_app + '/banner.html')) {
+    var wd_banner = fs.readFileSync(wd_dir + 'Apps/' + wd_app + '/banner.html');
+  }
+  else{
+    var wd_banner = '<div class="jumbotron"><h1>WebDesk-top</h1><p>The WebDesk for your desktop.</p></div>';
+  }
   if (fs.existsSync(wd_dir + 'Apps/' + wd_app + '/' + wd_sec + '.html')) {
     var wd_body = fs.readFileSync(wd_dir + 'Apps/' + wd_app + '/' + wd_sec + '.html');
   }
   else{
-    var wd_body = '<h1>404</h1><h3>Page not found.</h3>';
+    var wd_body = '<h1>404</h1><h3>Page not found.</h3><h4><a class="btn btn-primary" href="#" onclick="window.history.go(-1); return false;">Go Back</a></h4>';
   }
   if (fs.existsSync(wd_dir + 'Apps/' + wd_app + '/foot_' + wd_sec + '.html')) {
     var wd_foot = fs.readFileSync(wd_dir + 'Apps/' + wd_app + '/foot_' + wd_sec + '.html');
@@ -31,6 +40,7 @@ $(document).ready(function() {
     var wd_foot = '<script src="Plugins/bootstrap/dist/js/bootstrap.min.js"></script>';
   }
   document.getElementById("wd_head").innerHTML = wd_head;
+  document.getElementById("wd_banner").innerHTML = wd_banner;
   document.getElementById("wd_body").innerHTML = wd_body;
   document.getElementById("wd_foot").innerHTML = wd_foot;
   if (fs.existsSync(wd_dir + 'Apps/' + wd_app + '/' + wd_sec + '.js')) {
